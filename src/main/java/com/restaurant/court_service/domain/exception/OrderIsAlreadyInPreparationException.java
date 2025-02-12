0c1d1cc12470e7b10050443e87aceb00aff14a3f
Julian Ortiz <julian.ortiz@pragma.com.co>
2025-02-12 11:33:06 -0500

    endpoint for cancel order

---
+++ src/main/java/com/restaurant/court_service/domain/exception/OrderIsAlreadyInPreparationException.java
@@ -0,0 +1,7 @@
+package com.restaurant.court_service.domain.exception;
+
+public class OrderIsAlreadyInPreparation extends RuntimeException {
+    public OrderIsAlreadyInPreparation() {
+        super();
+    }
+}
